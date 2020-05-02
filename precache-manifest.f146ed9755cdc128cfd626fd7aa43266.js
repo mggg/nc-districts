f0129c9aea65268bce054999bfbe7cba1c78d669
Nick Doiron <ndoiron@mapmeld.com>
2020-05-02 13:33:06 -0400

    initial static deploy

---
+++ precache-manifest.f146ed9755cdc128cfd626fd7aa43266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facfb23ec2563cec4ea7e9e7b4ff8755",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f57df29af26331b360f",
+    "url": "/static/css/main.abc33920.chunk.css"
+  },
+  {
+    "revision": "6b2079531ea0ddbe4df9",
+    "url": "/static/js/2.429e1e3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.429e1e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f57df29af26331b360f",
+    "url": "/static/js/main.e0c838db.chunk.js"
+  },
+  {
+    "revision": "2c41e22f70569515be41",
+    "url": "/static/js/runtime-main.0bce63f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
